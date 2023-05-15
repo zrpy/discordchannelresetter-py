dd3d8ac89d32e322eb37c8566c0d865d2fb5b568
zrpy <91083604+zrpy@users.noreply.github.com>
2023-05-16 01:07:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime
 import pytz
 
 session = discord.Client(intents=discord.Intents.all())
+token=""
 guilds={}
 resettimes={
     '00:00',
@@ -21,9 +22,8 @@ resettimes={
 async def nuke_channels():
     nowtime = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%H:%M')
     if nowtime in resettimes:
-    for guildjson in guilds:
-      guilddata=guilds[guildjson]
-      guild=guild = session.get_guild(guilddata["id"])
+    for guildid in guilds:
+      guild = session.get_guild(int(guildid))
       for channel in guild.text_channels:
         if channel.topic==None:continue
         if 'channelresetter' in channel.topic:
@@ -32,5 +32,24 @@ async def nuke_channels():
           resetedchannel = await channel.clone()
           await resetedchannel.edit(position=position)
           embed = discord.Embed(title='チャンネルをリセットしました',description='サポートアカウント:[@whitehatpy](https://twitter.com/whitehatpy', timestamp=datetime.datetime.utcnow())
-          embed.set_footer(text=f"Produced by @whitehatpy", icon_url=client.user.avatar_url)
+          embed.set_footer(text=f"Produced by @whitehatpy", icon_url=session.user.avatar_url)
           await resetedchannel.send(embed=embed)
+
+@session.slash_command(name="setupnuke",description="チャンネルログ削除を設定するよ")
+@commands.has_guild_permissions(manage_guild=True,manage_channels=True)
+async def setupnuke(ctx):
+  if ctx.guild.id in guilds:
+    await ctx.respond("設定しています")
+    return
+  guilds[ctx.guild.id]={}
+  await ctx.respond("設定しました")
+
+@session.slash_command(name="unsetupnuke",description="チャンネルログ削除の設定を削除するよ")
+@commands.has_guild_permissions(manage_guild=True,manage_channels=True)
+async def unsetupnuke(ctx):
+  if　not ctx.guild.id in guilds:
+    await ctx.respond("データが無いです")
+    return
+  guilds.pop(ctx.guild.id)
+  await ctx.respond("データを削除しました")
+session.run(token)
