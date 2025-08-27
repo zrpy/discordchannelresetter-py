7c549ca840bcd25b377009b089c6d2d30286f5e7
zrpy <91083604+zrpy@users.noreply.github.com>
2025-08-28 00:45:48 +0900

    そもそも動かなかったことにきずいてなかったので、直した
    結構直した

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands,tasks
+from discord import app_commands
 import datetime
 import pytz
 import asyncio
+import re
 
-session = discord.Client(intents=discord.Intents.all())
+intents = discord.Intents.all() 
+session = discord.Client(intents=intents,allowed_mentions=discord.AllowedMentions(everyone=False, users=False, roles=False, replied_user=False))
+tree = app_commands.CommandTree(session)
 token=""
 guilds={}
-resettimes={
+normal_times={
     '00:00',
     '02:00',
     '04:00',
@@ -25,22 +29,22 @@ async def nuker(guildid):
     for channel in guild.text_channels:
         if not channel.topic==None:
             if 'channelresetter' in channel.topic:
-                time_list=re.findall(r'reset_time\[(\d+:\d+),(\d+:\d+)\]', channel.topic)
-                if len(times)==0:
-                    times=resettimes
-                else:
-                    times=time_list[0]
+                print(channel.topic)
+                times=re.search(r"reset_time\[(\d+:\d+)(?:,(\d+:\d+))*\]",channel.topic)
+                times_list=normal_times
+                if times:
+                    times_list = times.groups()
                 now = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%H:%M')
-                if now in times:
+                if now in times_list:
                     position = channel.position
                     await channel.delete()
                     resetedchannel = await channel.clone()
                     await resetedchannel.edit(position=position)
-                    embed = discord.Embed(title='チャンネルをリセットしました',description='サポートアカウント:[@whitehatpy](https://twitter.com/whitehatpy)', timestamp=datetime.datetime.utcnow())
-                    embed.set_footer(text=f"Produced by @whitehatpy", icon_url=session.user.avatar_url)
+                    embed = discord.Embed(title='チャンネルをリセットしました',description='サポートアカウント:[@zrpy0](https://x.com)', timestamp=datetime.datetime.now(datetime.UTC))
+                    embed.set_footer(text=f"Produced by @zrpy0", icon_url=session.user.display_avatar.url)
                     await resetedchannel.send(embed=embed)
 
-@tasks.loop(seconds=20)
+@tasks.loop(seconds=60)
 async def nukeloops():
     tasks=[]
     for guildid in guilds:
@@ -50,23 +54,24 @@ async def nukeloops():
 @session.event
 async def on_ready():
     nukeloops.start()
+    await tree.sync()
 
-@session.slash_command(name="add_reset",description="チャンネルログ削除を設定するよ")
+@tree.command(name="add_reset",description="チャンネルログ削除を設定するよ")
 @commands.has_guild_permissions(manage_guild=True,manage_channels=True)
 async def add_reset(ctx):
     if ctx.guild.id in guilds:
-        await ctx.respond("設定しています")
+        await ctx.response.send_message("設定しています")
         return
     guilds[ctx.guild.id]={}
-    await ctx.respond("設定しました")
+    await ctx.response.send_message("設定しました")
 
-@session.slash_command(name="remove_reset",description="チャンネルログ削除の設定を削除するよ")
+@tree.command(name="remove_reset",description="チャンネルログ削除の設定を削除するよ")
 @commands.has_guild_permissions(manage_guild=True,manage_channels=True)
 async def remove_reset(ctx):
     if not ctx.guild.id in guilds:
-        await ctx.respond("データが無いです")
+        await ctx.response.send_message("データが無いです")
         return
     guilds.pop(ctx.guild.id)
-    await ctx.respond("データを削除しました")
+    await ctx.response.send_message("データを削除しました")
 
 session.run(token)
